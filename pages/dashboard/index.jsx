ec8356ec741ecf229bd4348d7499d7d1a526d42c
ardhptr21 <ardhiputrapradana21@gmail.com>
2022-08-25 16:48:41 +0700

    showing stat

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -4,8 +4,9 @@ import { VscOrganization } from 'react-icons/vsc';
 import { HiOutlineUser } from 'react-icons/hi';
 import { AiOutlineNotification, AiOutlineDashboard } from 'react-icons/ai';
 import { authenticatedAdmin } from 'middleware/auth';
+import prisma from '@/lib/prisma';
 
-export default function Dashboard() {
+export default function Dashboard({ users, extracurriculars, requests }) {
   return (
     <LayoutDashboard
       description="Kelola dan lihat beberapa ringkasan statistik di dashboard"
@@ -13,12 +14,20 @@ export default function Dashboard() {
       icon={AiOutlineDashboard}
     >
       <section className="grid md:grid-cols-3 gap-5 grid-cols-1">
-        <CardStat icon={VscOrganization} title="Total Ekstrakulikuler" value="12" />
-        <CardStat icon={HiOutlineUser} title="Jumlah User" value="56" />
-        <CardStat icon={AiOutlineNotification} title="Permintaan Menunggu" value="12" />
+        <CardStat icon={VscOrganization} title="Total Ekstrakulikuler" value={extracurriculars} />
+        <CardStat icon={HiOutlineUser} title="Jumlah User" value={users} />
+        <CardStat icon={AiOutlineNotification} title="Permintaan Menunggu" value={requests} />
       </section>
     </LayoutDashboard>
   );
 }
 
-export const getServerSideProps = authenticatedAdmin(async () => ({ props: {} }));
+export const getServerSideProps = authenticatedAdmin(async () => {
+  const [users, extracurriculars, requests] = await Promise.all([
+    prisma.user.count({ where: { role: 'user' } }),
+    prisma.extracurricular.count(),
+    prisma.usersOnExtracurriculars.count({ where: { status: 'pending' } }),
+  ]);
+
+  return { props: { users, extracurriculars, requests } };
+});
